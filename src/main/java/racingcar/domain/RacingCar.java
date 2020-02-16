43d31937eddc9e08ae9174de211806aeedf18ea8
sihyung92 <fjzjqhdl@gmail.com>
2020-02-16 16:23:15 +0900

    feat : 이동 유무 판별기능 추가

---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+public class RacingCar {
+    public static boolean go(int number) {
+        if (number >= 4) {
+            return true;
+        }
+        return false;
+    }
+}
