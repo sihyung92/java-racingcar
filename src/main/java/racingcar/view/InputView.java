a8498a2e754208b35a702f7afe2a5cac4cde3d04
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-02-20 18:39:51 +0900

    feat : 입력값 유효성 검사 기능 추가
    -Test : test 코드 작성 완료

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,24 +5,45 @@ import racingcar.util.StringUtils;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class InputView {
     private final static Scanner scanner = new Scanner(System.in);
 
     private static final String INTRODUCE_INPUT_PLAYERS_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String QUESTION_INPUT_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String INPUT_PLAYERS_DELIMITER = ",";
-
-    private List<String> names;
+    private static final int ZERO = 0;
+    private static final int BOUND = 10;
 
     public List<String> inputNames() {
         System.out.println(INTRODUCE_INPUT_PLAYERS_MESSAGE);
-        return StringUtils.splitByDelimiterAsList(scanner.nextLine(), INPUT_PLAYERS_DELIMITER);
+        return StringUtils.splitByDelimiterAsList
+                (validateNames(scanner.nextLine()), INPUT_PLAYERS_DELIMITER);
     }
 
     public int inputCount() {
         System.out.println(QUESTION_INPUT_COUNT_MESSAGE);
-        return scanner.nextInt();
+        int count = scanner.nextInt();
+        return validateCount(count);
     }
 
+    public int validateCount(int count) {
+        if (isEmptyCount(count))
+            throw new IllegalArgumentException("1 ~ 9 사이의 숫자를 입력해주세요.");
+        return count;
+    }
 
+    public String validateNames(String input) {
+        if (isEmptyName(input))
+            throw new IllegalArgumentException("비어있는 이름은 입력할 수 없습니다.");
+        return input;
+    }
+
+    private boolean isEmptyCount(int count) {
+        return (count <= ZERO || count >= BOUND);
+    }
+
+    private boolean isEmptyName(String input) {
+        return (input == null || input.trim().equals(""));
+    }
 }
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -3,11 +3,14 @@ package racingcar.view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.util.StringUtils;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
 
@@ -19,5 +22,23 @@ public class InputViewTest {
         assertThat(names.size() <= 10).isEqualTo(expectedResult);
     }
 
+    @DisplayName("자동차 이름 검사 (공백인지)")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 자동차_이름_검사(String input) {
+        assertThatThrownBy(() -> { new InputView().validateNames(input); })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("비어있는 이름은 입력할 수 없습니다.");
+    }
+
+
+    @DisplayName("라운드 숫자 검사 (0이 아닌지)")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    void 라운드_숫자_검사(int i) {
+        assertThatThrownBy(() -> { new InputView().validateCount(i); })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 ~ 9 사이의 숫자를 입력해주세요.");
+    }
 
 }
